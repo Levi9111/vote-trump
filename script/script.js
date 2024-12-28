b1e5b9469fc630d49d21863e72cc2010d93daf59
Levi9111 <captainlevi9111@gmail.com>
2024-12-28 22:06:22 +0600

    responsive nav created

--- script/script.js
+++ script/script.js
@@ -1,5 +1,14 @@
-console.log(window.innerWidth);
+'use strict';
+const navBtn = document.querySelector('.nav-btn');
+const navUl = document.querySelector('.responsive-nav ul')
 
+navBtn.addEventListener('click', () => {
+    if (navUl.style.top === '60px') {
+        navUl.style.top = '-600px'; 
+      } else {
+        navUl.style.top = '60px'; 
+      }
+})
 // 1366 ---> 100%
 // 1517 ---> 90%
 // 1707 ---> 80%
